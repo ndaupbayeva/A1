884239a3e7734a06f003f7a529b2502766a096d6
Nuriya <230185@astanait.edu.kz>
2023-12-27 20:02:32 +0600

    Organized all files

--- src/models/Point.java
+++ src/models/Point.java
@@ -1,4 +1,20 @@
 package models;
 
 public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double findDistance(Point destination){ //creating a function to find the distance by formula
+        double distance = 0;
+        double dX = destination.x - this.x;
+        double dY = destination.x - this.y;
+        distance = Math.sqrt(dX * dX + dY * dY);
+        return distance;
+    }
+
 }
